d79afa898dccd93496e04f4b2129087674351f82
BastienVienet <bastien.vienet@outlook.com>
2022-12-02 13:06:56 +0100

    modifiying files to match the exam rules

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,7 @@ import random
 
 app = Flask(__name__)
 
-# list of funny meme
+# list of funny memes
 images = [
     "https://media.giphy.com/media/kusPzjBcmhc2yB06Tq/giphy.gif",
     "https://media.giphy.com/media/KEeyysnlLdJ4afgEhk/giphy.gif",
@@ -26,7 +26,7 @@ def hello():
 
 @app.route("/exam-docker")
 def exam_docker():
-    return "Hello ! This is a test page for the docker exam"
+    return "Hello ! This is a test for the docker exam"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
